c02aedbcf4ae7614d07d63423e0743ee596afff6
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-07-19 11:15:44 -0300

    adicionando geração de números aleatórios

--- Source/Arvores Rubro Negras/test_red_black_tree.c
+++ Source/Arvores Rubro Negras/test_red_black_tree.c
@@ -1,8 +1,9 @@
 #include"red_black_tree.h"
 #include<stdio.h>
 #include<ctype.h>
-#define MAX_INSERCAO 5000
-#define MAX_PESQUISA 100
+#define MAX_SIZE 10000
+#define SEARCH 1000
+#define MAX_VALUE 2000
 
 /*  this file has functions to test a red-black tree of integers */
 
@@ -33,38 +34,61 @@ void InfoDest(void *a){
 int main()
 {
   int i =0;
-  int* newInt;
+  int value, j;
+  srand(time(NULL));   // should only be called once
+  unsigned int r;
+  int *array;
+  array = (int*) malloc(sizeof(int)*MAX_SIZE);
 	
-  FILE *arq;
+   if(array == NULL)
+   {
+        printf("Vetor não foi inicializado.\n");
+        return 1;
+   }
+	
+  //FILE *arq;
   rb_red_blk_node* newNode;
   rb_red_blk_tree* tree;
 
   /*cria a arvore*/
   tree=RBTreeCreate(IntComp,IntDest,InfoDest,IntPrint,InfoPrint);
-  arq = fopen("file.txt", "r");
+  /*arq = fopen("file.txt", "r");
   if(arq == NULL)
   {
     printf("Erro na abertura dos numeros\n");
-  }
+  }*/
 
 
  /*insercao na arvore*/
-  while(i<MAX_INSERCAO)
+  for(i = 0; i < MAX_SIZE; i++){ //gera números aleatórios
+        r = (rand()%MAX_VALUE)+1;
+        array[i] = r;
+        RBTreeInsert(tree, &r, (void*)1);
+  }
+	/*
+  while(i<MAX_SIZE)
   {
     newInt=(int*) malloc(sizeof(int));
     fscanf(arq, "%d", newInt);
-   /* printf("%d - ", *newInt);*/
-    RBTreeInsert(tree,newInt,(void*)1);
+    printf("%d - ", *newInt);
+    
     i++;
   }
-
-  i=0;
+	*/
+  for(i = 0; i < SEARCH; i++){
+        value = array[(rand()%MAX_SIZE)];
+        if(( newNode = RBExactQuery(tree, &value) ) == NULL) {
+                printf("Erro ao pesquisar na arvore.\n");
+                return -1;
+        }
+  }
+	/*
   while(i<MAX_PESQUISA)
   {	
-    /*newInt=(int*) malloc(sizeof(int));*/
+    newInt=(int*) malloc(sizeof(int));
     fscanf(arq, "%d", newInt);
     printf("%d - ", *newInt);
-    if ( ( newNode = RBExactQuery(tree,newInt) ) != NULL) {/*assignment*/
+    if ( ( newNode = RBExactQuery(tree,newInt) ) != NULL) {assignment
       printf("data found in tree at location %i\n",(int)newNode);
     } else {
       printf("data not in tree\n");
@@ -73,18 +97,8 @@ int main()
   }
 
   fclose(arq);
-    
+ */   
    /* RBTreePrint(tree);*/
-
+	printf("Processo concluido com sucesso.\n");
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
